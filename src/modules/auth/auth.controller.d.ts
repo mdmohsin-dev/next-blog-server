952ff2c7c674b1c89bdb8c1993b35746ae293f95
md mohosin <mdmohosin.web@gmail.com>
2026-06-14 13:15:55 +0600

    update file and folder

--- src/modules/auth/auth.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Request, Response } from "express";
-export declare const AuthController: {
-    loginWithEmailAndPassword: (req: Request, res: Response) => Promise<void>;
-    authWithGoogle: (req: Request, res: Response) => Promise<void>;
-};
-//# sourceMappingURL=auth.controller.d.ts.map
--- src/modules/auth/auth.route.js
+++
@@ -1,7 +0,0 @@
-import express, { Router } from 'express';
-import { AuthController } from './auth.controller';
-const router = express.Router();
-router.post("/login", AuthController.loginWithEmailAndPassword);
-router.post("/google", AuthController.authWithGoogle);
-export const AuthRouter = router;
-//# sourceMappingURL=auth.route.js.map
