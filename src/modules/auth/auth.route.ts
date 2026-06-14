3872674ebaaac67c314246a768ead993c64dd0c1
md mohosin <mdmohosin.web@gmail.com>
2026-06-14 14:38:20 +0600

    add file

---
+++ src/modules/auth/auth.route.ts
@@ -0,0 +1,15 @@
+import express, { Router } from 'express';
+import { AuthController } from './auth.controller';
+
+const router = express.Router();
+
+router.post(
+    "/login",
+    AuthController.loginWithEmailAndPassword
+)
+router.post(
+    "/google",
+    AuthController.authWithGoogle
+)
+
+export const AuthRouter: Router = router;
---
+++ src/modules/user/user.route.ts
@@ -0,0 +1,31 @@
+import express, { Router } from 'express';
+import { UserController } from './user.controller';
+
+const router = express.Router();
+
+router.get(
+    "/",
+    UserController.getAllFromDB
+)
+
+router.get(
+    "/:id",
+    UserController.getUserById
+)
+
+router.post(
+    "/",
+    UserController.createUser
+)
+
+router.patch(
+    "/:id",
+    UserController.updateUser
+)
+
+router.delete(
+    "/:id",
+    UserController.deleteUser
+)
+
+export const UserRouter:Router = router;
